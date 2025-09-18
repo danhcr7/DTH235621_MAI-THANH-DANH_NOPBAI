741f379390e1860aca1f4d0b1742140724599a72
danhcr7 <148963835+danhcr7@users.noreply.github.com>
2025-09-18 09:59:08 +0700

    Add files via upload

---
+++ Chuong2/Cau7-C2.py
@@ -0,0 +1,10 @@
+"""
+1 số cách nhập dữ liệu từ bàn phím:
+a) Nhập trên nhiều dòng:
+x = int(input())
+y = float(input())
+z = intput()
+b) 1 dòng:
+a, b, c = map(int, input().split())
+x, y, z = map(float, input().split())
+"""
