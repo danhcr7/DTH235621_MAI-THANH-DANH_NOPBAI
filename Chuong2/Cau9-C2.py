cda0c2273d7744a48524254e60a7cf666e9da298
danhcr7 <148963835+danhcr7@users.noreply.github.com>
2025-09-18 10:18:20 +0700

    Add files via upload

---
+++ Chuong2/Cau9-C2.py
@@ -0,0 +1,18 @@
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+# i1 + (i2 * i3): lấy i2 * i3 trước ra -15 rồi cộng i1 = 2 ra -13
+print(i1+(i2*i3))
+# i1 * (i2 + i3): lấy i2 + i3 ra 2 rồi nhân với i1 = 2 ra 4
+print(i1*(i2+i3))
+# i1 / (i2 + i3): lấy i2 + i3 ra 2. lấy i1 = 2 chia thập phân cho 2 ra 1.0 
+print(i1/(i2+i3))
+#) i1 // (i2 + i3): lấy i2 + i3 ra 2. lấy i1 = 2 chia nguyên cho 2 ra 1  
+print(i1//(i2+i3))
+# i1 / i2 + i3: lấy i1 chia thập phân cho i2 ra 0.4 rồi cộng chi i3 = -3 ra -2.6
+print(i1/i2 + i3)
+
+ 
